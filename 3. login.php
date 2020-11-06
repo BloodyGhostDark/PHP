a5ade4608c53775b42a2670bfc37dcb4261afb4f
luyumingqiang <73977418+BloodyGhostDark@users.noreply.github.com>
2020-11-06 14:22:40 +0800

    Update 3. login.php

--- 3. login.php
+++ 3. login.php
@@ -1,6 +1,6 @@
 <?php 
     # 如果你輸入帳號,密碼跟系統的帳號,密碼是一樣的 
-    if (($_GET[id] == "john") && ($_GET[pwd]=="john1234"))
+    if (($_GET['id'] == "john") && ($_GET['pwd']=="john1234"))
         echo "Welcome";# true 會出現welcome
     else
         echo "fail login";# false 會出現 fail login 
